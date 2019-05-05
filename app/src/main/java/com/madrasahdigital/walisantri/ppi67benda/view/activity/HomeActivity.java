ad962ddcae65dc0c9048b6d5066695898844ea75
alhudaghifari <alhudaghifari@gmail.com>
2019-05-06 06:28:42 +0700

    home layout edited

--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/HomeActivity.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/HomeActivity.java
@@ -2,16 +2,26 @@ package com.madrasahdigital.walisantri.ppi67benda.view.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.madrasahdigital.walisantri.ppi67benda.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private final String TAG = HomeActivity.class.getSimpleName();
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.name_example, R.layout.spinner_text);
+        spinner.setAdapter(adapter);
     }
 
 }
--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/LoginActivity.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.madrasahdigital.walisantri.ppi67benda.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.madrasahdigital.walisantri.ppi67benda.R;
 
@@ -11,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void gotoHomePage(View view) {
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
