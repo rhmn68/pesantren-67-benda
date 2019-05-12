992458170dbe190d0e78044db883c0ede4051abd
alhudaghifari <alhudaghifari@gmail.com>
2019-05-12 13:38:19 +0700

    library added in gradle

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/service/RetrofitClientUtils.java
@@ -0,0 +1,7 @@
+package com.madrasahdigital.walisantri.ppi67benda.service;
+
+/**
+ * Created by Alhudaghifari on 13:30 12/05/19
+ */
+public class RetrofitClientUtils {
+}
---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/service/RetrofitServices.java
@@ -0,0 +1,7 @@
+package com.madrasahdigital.walisantri.ppi67benda.service;
+
+/**
+ * Created by Alhudaghifari on 13:29 12/05/19
+ */
+public class RetrofitServices {
+}
