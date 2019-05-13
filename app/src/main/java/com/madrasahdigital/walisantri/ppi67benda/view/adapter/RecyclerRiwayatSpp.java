ccb958fe4c8ea3e4829697fc5b97c44e962af1c8
alhudaghifari <alhudaghifari@gmail.com>
2019-05-13 22:56:54 +0700

    riwayat spp done

--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/adapter/RecyclerRiwayatSpp.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/adapter/RecyclerRiwayatSpp.java
@@ -1,7 +1,122 @@
 package com.madrasahdigital.walisantri.ppi67benda.view.adapter;
 
+import android.content.Context;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.madrasahdigital.walisantri.ppi67benda.R;
+import com.madrasahdigital.walisantri.ppi67benda.model.riwayatspp.RiwayatSpp;
+import com.madrasahdigital.walisantri.ppi67benda.utils.UtilsManager;
+
+import java.util.List;
+
 /**
  * Created by Alhudaghifari on 22:21 13/05/19
  */
-public class RecyclerRiwayatSpp {
+public class RecyclerRiwayatSpp extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static String TAG = RecyclerRiwayatSpp.class.getSimpleName();
+    private List<RiwayatSpp> riwayatSppList;
+    private Context mContext;
+    private OnArtikelClickListener mOnArtikelClickListener;
+
+    public RecyclerRiwayatSpp(Context context, List<RiwayatSpp> riwayatSpps) {
+        mContext = context;
+        riwayatSppList = riwayatSpps;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view;
+        view = LayoutInflater.from(mContext).inflate(R.layout.item_riwayat_spp, viewGroup, false);
+        return new ViewHolderCategory(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int i) {
+        ViewHolderCategory viewHolderCategory = (ViewHolderCategory) holder;
+        final int position = i;
+        final RiwayatSpp riwayatSpp = riwayatSppList.get(i);
+
+        String date = UtilsManager.getMonthFromNumber(mContext, riwayatSpp.getBulan())
+                        + " " + riwayatSpp.getPeriode();
+        String status;
+        if (riwayatSpp.getStatus()) {
+            status = mContext.getResources().getString(R.string.lunas);
+            viewHolderCategory.ivCheck.setImageResource(R.drawable.ic_check_circle_green_24dp);
+        } else {
+            status = mContext.getResources().getString(R.string.belumdibayar);
+            viewHolderCategory.ivCheck.setImageResource(R.drawable.ic_add_circle_red_24dp);
+            viewHolderCategory.ivCheck.setRotation(45);
+        }
+
+        viewHolderCategory.tvPeriode.setText(date);
+        viewHolderCategory.tvStatus.setText(status);
+        viewHolderCategory.tvNominal.setText(UtilsManager.convertLongToCurrencyIDv2WithoutRp(Double.parseDouble(riwayatSpp.getNominal())));
+
+        viewHolderCategory.mViewContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOnArtikelClickListener != null) {
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (mOnArtikelClickListener != null)
+                                mOnArtikelClickListener.onClick(position, riwayatSpp);
+                        }
+                    }, 250);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return riwayatSppList.size();
+    }
+
+    private class ViewHolderCategory extends RecyclerView.ViewHolder {
+        public View mViewContainer;
+
+        public CardView cardView;
+        public TextView tvPeriode;
+        public TextView tvStatus;
+        public TextView tvNominal;
+        public ImageView ivCheck;
+
+        public ViewHolderCategory(View itemView) {
+            super(itemView);
+            mViewContainer = itemView;
+            cardView = itemView.findViewById(R.id.cardView);
+            tvPeriode = itemView.findViewById(R.id.tvPeriode);
+            tvStatus = itemView.findViewById(R.id.tvStatus);
+            tvNominal = itemView.findViewById(R.id.tvNominal);
+            ivCheck = itemView.findViewById(R.id.ivCheck);
+        }
+    }
+
+    /**
+     * interface ketika container di click
+     */
+    public interface OnArtikelClickListener {
+        void onClick(int posisi, RiwayatSpp riwayatSpp);
+    }
+
+    /**
+     * listener artikel di klik
+     *
+     * @param onArtikelClickListener onartikelclicklistener
+     */
+    public void setOnArtikelClickListener(OnArtikelClickListener onArtikelClickListener) {
+        mOnArtikelClickListener = onArtikelClickListener;
+    }
 }
--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/fragment/RiwayatSppFragment.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/fragment/RiwayatSppFragment.java
@@ -1,30 +1,147 @@
 package com.madrasahdigital.walisantri.ppi67benda.view.fragment;
 
 
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
 import com.madrasahdigital.walisantri.ppi67benda.R;
+import com.madrasahdigital.walisantri.ppi67benda.model.riwayatspp.RiwayatSpp;
+import com.madrasahdigital.walisantri.ppi67benda.utils.Constant;
+import com.madrasahdigital.walisantri.ppi67benda.utils.SharedPrefManager;
+import com.madrasahdigital.walisantri.ppi67benda.utils.UtilsManager;
+import com.madrasahdigital.walisantri.ppi67benda.view.adapter.RecyclerRiwayatSpp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SpecificFragment extends Fragment {
+public class RiwayatSppFragment extends Fragment {
 
+    private final String TAG = RiwayatSppFragment.class.getSimpleName();
+    private RecyclerRiwayatSpp recyclerListChat;
+    private RecyclerRiwayatSpp.OnArtikelClickListener onArtikelClickListener;
+    private List<RiwayatSpp> riwayatSppList;
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private SharedPrefManager sharedPrefManager;
+    private boolean isThreadWork = false;
 
-    public SpecificFragment() {
+    public RiwayatSppFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_specific, container, false);
+        View v = inflater.inflate(R.layout.fragment_riwayat_spp, container, false);
+
+        mRecyclerView = v.findViewById(R.id.rv_numbers);
+        swipeRefreshLayout = v.findViewById(R.id.swipeRefresh);
+        sharedPrefManager = new SharedPrefManager(getContext());
+        riwayatSppList = new ArrayList<>();
+
+        swipeRefreshLayout.setColorSchemeColors(Color.GREEN, Color.BLUE, Color.MAGENTA);
+
+        initializationOfListener();
+        new GetRiwayatSpp().execute();
+
+        return v;
     }
 
+    private void initializationOfListener() {
+        onArtikelClickListener = new RecyclerRiwayatSpp.OnArtikelClickListener() {
+            @Override
+            public void onClick(int posisi, RiwayatSpp riwayatSpp) {
+
+            }
+        };
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (!isThreadWork)
+                    new GetRiwayatSpp().execute();
+            }
+        });
+    }
+
+    private void initializationOfViewer() {
+        final LinearLayoutManager mLinearLayoutManager =
+                new LinearLayoutManager(RiwayatSppFragment.this.getActivity());
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        recyclerListChat = new RecyclerRiwayatSpp(getContext(), riwayatSppList);
+        recyclerListChat.setOnArtikelClickListener(onArtikelClickListener);
+
+        mRecyclerView.setAdapter(recyclerListChat);
+    }
+
+    private class GetRiwayatSpp extends AsyncTask<Void, Integer, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            isThreadWork = true;
+            swipeRefreshLayout.setRefreshing(true);
+            riwayatSppList = new ArrayList<>();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(Constant.LINK_GET_RIWAYAT_SPP)
+                    .get()
+                    .addHeader("Authorization", sharedPrefManager.getToken())
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+
+                ResponseBody responseBody = response.body();
+                String bodyString = responseBody.string();
+
+                Gson gson = new Gson();
+                RiwayatSpp[] paymentModel =
+                        gson.fromJson(bodyString, RiwayatSpp[].class);
+
+                riwayatSppList.addAll(Arrays.asList(paymentModel));
+
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean isSuccess) {
+            swipeRefreshLayout.setRefreshing(false);
+            isThreadWork = false;
+            if (isSuccess) {
+                initializationOfViewer();
+            } else {
+                UtilsManager.showToast(getContext(), getResources().getString(R.string.cekkoneksi));
+            }
+        }
+    }
 }
