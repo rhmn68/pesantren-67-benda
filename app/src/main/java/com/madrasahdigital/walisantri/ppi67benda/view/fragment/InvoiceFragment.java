7a5dc5d3fa8873954495abe2c2f8e69d60608464
alhudaghifari <alhudaghifari@gmail.com>
2019-05-13 14:16:39 +0700

    fragment spp added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/fragment/InvoiceFragment.java
@@ -0,0 +1,30 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.madrasahdigital.walisantri.ppi67benda.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InvoiceFragment extends Fragment {
+
+
+    public InvoiceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invoice, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/fragment/SpecificFragment.java
@@ -0,0 +1,30 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.madrasahdigital.walisantri.ppi67benda.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SpecificFragment extends Fragment {
+
+
+    public SpecificFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specific, container, false);
+    }
+
+}
