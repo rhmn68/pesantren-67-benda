91e550924ee413ff7966051c75a20564ff303378
alhudaghifari <alhudaghifari@gmail.com>
2019-05-22 06:12:33 +0700

    some item created

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/MakePaymentActivity.java
@@ -0,0 +1,13 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakePaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_payment);
+    }
+}
---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/adapter/RecyclerMakePayment.java
@@ -0,0 +1,7 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.adapter;
+
+/**
+ * Created by Alhudaghifari on 5:41 22/05/19
+ */
+public class RecyclerMakePayment {
+}
