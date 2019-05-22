221c0a4e424bc2277a9bb766bfc9a30ea963c588
alhudaghifari <alhudaghifari@gmail.com>
2019-05-22 09:03:54 +0700

    some file added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/dialog/LogoutDialog.java
@@ -0,0 +1,83 @@
+package com.neurafarm.sobattania.view.dialog;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.neurafarm.sobattania.R;
+
+
+/**
+ * Created by ghifar on 09/03/18.
+ */
+
+public class LogoutDialog extends Dialog implements
+        android.view.View.OnClickListener {
+
+    private Activity myActivity;
+    private Button btnLogout;
+    private Button btnTidak;
+
+    private TextView tvQuestion;
+
+    OnMyDialogResult mDialogResult;
+
+    public LogoutDialog(Activity context) {
+        super(context);
+        myActivity = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.dialog_logout);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        btnLogout = findViewById(R.id.btn_logout);
+        btnTidak = findViewById(R.id.btn_tidak);
+        btnLogout.setOnClickListener(this);
+        btnTidak.setOnClickListener(this);
+        tvQuestion = (TextView) findViewById(R.id.tv_question);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_logout:
+                if (mDialogResult != null) {
+                    mDialogResult.finish(true);
+                }
+                dismiss();
+
+                break;
+            case R.id.btn_tidak:
+                if (mDialogResult != null) {
+                    mDialogResult.finish(false);
+                }
+                dismiss();
+
+                break;
+            default:
+                break;
+        }
+        dismiss();
+    }
+
+    public void setDialogResult(OnMyDialogResult dialogResult) {
+        mDialogResult = dialogResult;
+    }
+
+    public void setTvQuestion(String question) {
+        tvQuestion.setText(question);
+    }
+
+    public interface OnMyDialogResult {
+        void finish(boolean result);
+    }
+}
---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/dialog/SettingDialog.java
@@ -0,0 +1,7 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.dialog;
+
+/**
+ * Created by Alhudaghifari on 8:31 22/05/19
+ */
+public class SettingDialog {
+}
