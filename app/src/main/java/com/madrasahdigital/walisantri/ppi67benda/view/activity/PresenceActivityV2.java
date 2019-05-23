3fb2ae9d0c01c17323e9e373679cae8898077291
alhudaghifari <alhudaghifari@gmail.com>
2019-05-23 23:29:07 +0700

    presence activity v2 added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/PresenceActivityV2.java
@@ -0,0 +1,14 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresenceActivityV2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_presence_v2);
+    }
+}
