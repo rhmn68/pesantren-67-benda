7f3f209b7444b656169570bad6f61a7a56cab47b
alhudaghifari <alhudaghifari@gmail.com>
2019-06-13 13:40:43 +0700

    styles, strings, and dimens modified

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/addsantri/AddSantriActivity.java
@@ -0,0 +1,14 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity.addsantri;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddSantriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_santri);
+    }
+}
