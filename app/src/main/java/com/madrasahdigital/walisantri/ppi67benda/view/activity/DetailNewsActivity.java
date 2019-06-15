d610c98d1b23a34a2dcd7e874434f832d6ce3b18
alhudaghifari <alhudaghifari@gmail.com>
2019-06-15 14:22:24 +0700

    detail news modified

--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/DetailNewsActivity.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/DetailNewsActivity.java
@@ -1,16 +1,34 @@
-package com.madrasahdigital.walisantri.ppi67benda.view.activity.addsantri;
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import com.madrasahdigital.walisantri.ppi67benda.R;
 
 public class DetailNewsActivity extends AppCompatActivity {
 
+    private ActionBar aksibar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_news);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.black));
+        toolbar.setTitle(getResources().getString(R.string.tambahkan_santri));
+        setSupportActionBar(toolbar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(ContextCompat
+                    .getColor(DetailNewsActivity.this, R.color.colorPrimaryDark));
+        }
+
+        aksibar = DetailNewsActivity.this.getSupportActionBar();
+        assert aksibar != null;
+        aksibar.setDisplayHomeAsUpEnabled(true);
     }
 }
