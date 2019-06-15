37b8b7d03f9961060c7192c24219ddd856a980ee
alhudaghifari <alhudaghifari@gmail.com>
2019-06-15 14:42:57 +0700

    choose santri added and file moved

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/presence/ChooseSantriPresenceActivity.java
@@ -0,0 +1,16 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.madrasahdigital.walisantri.ppi67benda.R;
+
+public class ChooseSantriPresenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_santri_presence);
+    }
+}
