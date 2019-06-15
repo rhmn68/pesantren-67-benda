585a7f7adb826209ac8ef607e65f14f3e6b2eb23
alhudaghifari <alhudaghifari@gmail.com>
2019-06-15 14:22:00 +0700

    detail news added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/DetailNewsActivity.java
@@ -0,0 +1,16 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity.addsantri;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.madrasahdigital.walisantri.ppi67benda.R;
+
+public class DetailNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_news);
+    }
+}
