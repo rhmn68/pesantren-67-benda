e9b4fbfcd5a699d0ce1f94d382674f4ec75b8a9b
alhudaghifari <alhudaghifari@gmail.com>
2019-06-15 20:13:56 +0700

    some file added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/NewsFromPesantrenActivity.java
@@ -0,0 +1,13 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsFromPesantrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_from_pesantren);
+    }
+}
