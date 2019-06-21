04d49d5738316990d680e3a29a77b69f71f08191
alhudaghifari <alhudaghifari@gmail.com>
2019-06-22 06:58:32 +0700

    welcome slide added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/WelcomeGuideActivity.java
@@ -0,0 +1,14 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_guide);
+    }
+}
