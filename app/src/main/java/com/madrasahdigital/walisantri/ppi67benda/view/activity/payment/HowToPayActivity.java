7d62181df0c8390e774ea33186db249912408215
alhudaghifari <alhudaghifari@gmail.com>
2019-07-26 15:15:04 +0700

    manifest modified

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/payment/HowToPayActivity.java
@@ -0,0 +1,14 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity.payment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HowToPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_pay);
+    }
+}
