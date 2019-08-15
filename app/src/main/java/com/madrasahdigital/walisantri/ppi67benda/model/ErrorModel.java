664411cc8cc65ea15922183558907e08fec5df41
alhudaghifari <alhudaghifari@gmail.com>
2019-08-15 14:37:08 +0700

    error model added

--- app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/model/ErrorModel.java
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/model/ErrorModel.java
@@ -1,7 +1,32 @@
 package com.madrasahdigital.walisantri.ppi67benda.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Alhudaghifari on 14:06 13/08/19
  */
 public class ErrorModel {
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("message")
+    @Expose
+    private String message;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
