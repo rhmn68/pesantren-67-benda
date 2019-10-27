c336cb817d17d26dcc3c69ce8c6ee57a0c034c9b
alhudaghifari <alhudaghifari@gmail.com>
2019-10-28 03:50:17 +0700

    profil pesantren activity added

---
+++ app/src/main/java/com/madrasahdigital/walisantri/ppi67benda/view/activity/ProfilPesantrenActivity.java
@@ -0,0 +1,13 @@
+package com.madrasahdigital.walisantri.ppi67benda.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfilPesantrenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_pesantren);
+    }
+}
